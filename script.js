76b87c8aa42a6b4feb6a291426dc2d287957b873
Ivirx <scorpianva@gmail.com>
2023-02-13 17:12:00 +0530

    site is complete

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
-let windowWidth = window.innerWidth;
-let translateBy = Math.min((windowWidth / 100) * 35, 250);
-let allImagesWidth = document.querySelector('.all-images').offsetWidth;
-
-let allImages = document.querySelector('.all-images').children;
+const allImagesElement = document.querySelector('.all-images');
+let centerAllImages = allImagesElement.offsetWidth / 2;
+let allImages = allImagesElement.children;
 let onImage = 0;
 
-Array.from(allImages).forEach((imageDiv, index) => {
-    imageDiv.style.transform =
-        ('translateX(' + String((allImagesWidth / 2) - (imageDiv.offsetWidth / 2)) + 'px)')
-    if (onImage === index) {
-        imageDiv.classList.remove('faint')
-    } else {
-        imageDiv.classList.add('faint')
-    }
-});
-
-const handle = (howMuch) => {
-    onImage = howMuch;
+function handleSlide() {
     Array.from(allImages).forEach((imageDiv, index) => {
-        imageDiv.style.transform = ('translateX(-' + String(((imageDiv.offsetWidth + 48) * howMuch)) + 'px)')
-        console.log(imageDiv.getBoundingClientRect().left, (imageDiv.offsetWidth + 48) * howMuch)
-        if (onImage === index) {
-            imageDiv.classList.remove('faint')
-        } else {
-            imageDiv.classList.add('faint')
-        }
+        imageDiv.style.transform =
+            ('translateX(' + String((centerAllImages - (imageDiv.offsetWidth / 2)) + (-1 * ((imageDiv.offsetWidth + 48) * onImage))) + 'px)');
+        if (onImage === index)
+            imageDiv.classList.remove('faint');
+        else
+            imageDiv.classList.add('faint');
     });
 }
 
-
 window.addEventListener('resize', () => {
-    windowWidth = window.innerWidth;
-    translateBy = Math.min((windowWidth / 100) * 35, 250);
-    allImagesWidth = document.querySelector('.all-images').offsetWidth;
+    centerAllImages = document.querySelector('.all-images').offsetWidth / 2;
+    handleSlide();
 });
 
 const previous = () => {
     if (onImage > 0) {
-        console.log('previous');
-        handle(onImage - 1);
+        onImage -= 1
+        handleSlide();
     }
 }
 
 const next = () => {
-    if (onImage <= 4) {
-        console.log('next');
-        handle(onImage + 1);
+    if (onImage < 5) {
+        onImage += 1
+        handleSlide();
     }
-
 }
+
+handleSlide();
